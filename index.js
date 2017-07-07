3a0db3a81e6aeda9a5f6cfd9e52799ffe409b650
Matthew Butler <matthewtbutler@gmail.com>
2017-07-07 10:05:04 -0500

    ipc communication
    also, ffmpeg

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const electron = require('electron')
+const ffmpeg = require('fluent-ffmpeg')
 
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, ipcMain } = electron
+//const BrowserWindow = electron.BrowserWindow
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video length is: ', metadata.format.duration)
+  })
 })
